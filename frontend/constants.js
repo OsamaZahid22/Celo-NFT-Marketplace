f6c56049dad38c60b13a9d99ebc11a99ab51c10c
Osama <osamazahid689@gmail.com>
2022-09-03 08:01:30 +0500

    first commit

---
+++ frontend/constants.js
@@ -0,0 +1,4 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/osamazahid22/celo-nft-marketplace";
+
+  export const MARKETPLACE_ADDRESS = " 0xD590A878AA8674fC5ca94CAef2E037Da4e1017A0";
---
+++ frontend/pages/_app.js
@@ -0,0 +1,67 @@
+import "@rainbow-me/rainbowkit/styles.css";
+import "../styles/globals.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+
+//Now, we will create a configuration object for the Celo Alfajores chain, as required by RainbowKit/wagmi
+const celoChain = {
+  id: 44787,
+  name: "Celo Alfajores Testnet",
+  network: "alfajores",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Celo",
+    symbol: "CELO",
+  },
+  rpcUrls: {
+    default: "https://alfajores-forno.celo-testnet.org",
+  },
+  blockExplorers: {
+    default: {
+      name: "CeloScan",
+      url: "https://alfajores.celoscan.io",
+    },
+  },
+  testnet: true,
+};
+
+//Now, we will configure the providers and connectors, which will let RainbowKit know how to interact with the chain
+
+const { chains, provider } = configureChains(
+  [celoChain],
+  [
+    jsonRpcProvider({
+      rpc: (chain) => {
+        if (chain.id !== celoChain.id) return null;
+        return { http: chain.rpcUrls.default };
+      },
+    }),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Celo NFT Marketplace",
+  chains,
+});
+
+//initialize a wagmi client that combines all the above information, that RainbowKit will use under the hood.
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
